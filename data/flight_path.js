cb862ede52a97a70fc53521a14e3787cf3fa692f
Andrea Lloyd <acl97mormon@tamu.edu>
2018-12-07 19:10:45 -0600

    moving images

--- data/flight_path.js
+++ data/flight_path.js
@@ -1,10 +1,12 @@
 var flightPoints = [
   [
     42.3656,
-    -71.0096
+    -71.0096,
+    //"Boston International Airport"
   ],
 
   [
     29.6459,
-    -95.2769
+    -95.2769,
+    //"Hobby Airport"
   ]]
