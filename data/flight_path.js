b9c73ef7b7ea2ade1ff5c75f54351e9a4378f1eb
Andrea Lloyd <acl97mormon@tamu.edu>
2018-12-07 20:23:02 -0600

    flight data alerted to include nashville

--- data/flight_path.js
+++ data/flight_path.js
@@ -2,11 +2,15 @@ var flightPoints = [
   [
     42.3656,
     -71.0096,
-    //"Boston International Airport"
+    //"Boston International Airport, Massachuetts"
   ],
-
+  [
+    36.1627,
+    -86.7816,
+    //"Nashville, Tennessee"
+  ],
   [
     29.6459,
     -95.2769,
-    //"Hobby Airport"
+    //"Hobby Airport, Texas"
   ]]
